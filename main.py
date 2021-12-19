d5f207fbfd6b4b418917db3cd661b3d261dd5118
guruprasadv22 <guruprasadv22@gmail.com>
2021-12-19 17:41:30 +0530

    modular init of global vars

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ class DrawInformation:
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
 
+    SIDE_PAD = 100
+    TOP_PAD = 150
+
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
@@ -18,3 +21,13 @@ class DrawInformation:
         self.window = pygame.display.set_mode((width, height))
         pygame.display.set_caption("Sorting Algorithm Visualization")
         self.set_list(lst)
+
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = ((self.height - self.TOP_PAD) /
+                             (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
